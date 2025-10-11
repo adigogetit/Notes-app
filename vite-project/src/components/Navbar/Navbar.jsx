23f0e937ed987420c13167659507391641466702
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2025-10-11 20:14:39 +0530

    search bar done

--- vite-project/src/components/Navbar/Navbar.jsx
+++ vite-project/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Profileinfo from '../Cards/Profileinfo'
 import { useNavigate } from 'react-router-dom'
+import Searchbar from '../searchbar/Searchbar';
 
 const Navbar = () => {
 
-  const navigate = useNavigate;
+  const [searchQuery , setSearchQuery] = useState("");
+  const navigate = useNavigate();
 
   const onLogout = ()=>{
     navigate("/login");
   }
+
+  const handleSearch = ()=>{};
+  const onClearSearch = ()=>{
+    setSearchQuery("");
+  };
+
+
   return (
     <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
         <h2 className='text-3xl font-medium text-black py-2'>Notes</h2>
+
+        <Searchbar 
+         value={searchQuery}
+         onChange={({target})=>{
+          setSearchQuery(target.value);
+         }}
+         handleSearch={handleSearch}
+         onClearSearch={onClearSearch}
+        />
+
         <Profileinfo onLogout={onLogout}/>
       
     </div>
