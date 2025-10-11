e3a2ce5406f11c26b59a28c0694a180518bdc4f8
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2025-10-11 16:26:35 +0530

    made full login page with password input

--- vite-project/src/components/Navbar/Navbar.jsx
+++ vite-project/src/components/Navbar/Navbar.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
-        <h2 className='text-3xl font-medium text-black py-2 m-auto'> Notes </h2>
+        <h2 className='text-3xl font-medium text-black py-2 m-auto'>Notes</h2>
+      
     </div>
-    
   )
 }
 
