c5ebe9d09526395a25b447d1e014fc306d1e71a4
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2025-10-11 19:38:29 +0530

    navbar profile card

--- vite-project/src/components/Navbar/Navbar.jsx
+++ vite-project/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Profileinfo from '../Cards/Profileinfo'
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+
+  const navigate = useNavigate;
+
+  const onLogout = ()=>{
+    navigate("/login");
+  }
   return (
     <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
-        <h2 className='text-3xl font-medium text-black py-2 m-auto'>Notes</h2>
+        <h2 className='text-3xl font-medium text-black py-2'>Notes</h2>
+        <Profileinfo onLogout={onLogout}/>
       
     </div>
   )
